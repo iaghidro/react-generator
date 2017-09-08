25079ef90e3edc59b7d58e3c065752f1b65682b7
iaghidro <steveghidro@quickenloans.com>
2017-09-08 13:26:33 -0400

    added react component and test template

---
+++ generators/app/templates/src/js/react/component/MyComponent.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Alert } from 'react-bootstrap';
+import common from 'ui-react-common';
+
+import * as selectors from './state-selectors/index';
+import * as actions from './actions/index';
+
+export class MyComponent extends Component {
+
+    render() {
+
+        return (
+                <div id="connectedComponentName">
+                </div>
+                );
+    }
+
+}
+
+MyComponent.propTypes = {
+
+};
+
+const mapStateToProps = (state) => {
+    return {
+//        firstItem: selectors.getFirstItem(state),
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+//        firstAction: (firstParam) => dispatch(actions.doFirstAction(firstParam)),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyComponent);
