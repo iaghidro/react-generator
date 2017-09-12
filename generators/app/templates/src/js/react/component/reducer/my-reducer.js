45824cf758abad0742e6f4d5b979dd498ba480ac
iaghidro <steveghidro@quickenloans.com>
2017-09-12 16:37:17 -0400

    added reducer, reducer test, and types

---
+++ generators/app/templates/src/js/react/component/reducer/my-reducer.js
@@ -0,0 +1,30 @@
+import * as types from './my-component-types';
+
+const defaultState = {
+    hasError: false,
+    error: null
+};
+
+const reducer = (state = defaultState, action) => {
+    const {
+        hasError,
+        error
+    } = state;
+
+    switch (action.type) {
+
+        case types.FAILED:
+            return {
+                hasError: true,
+                error: action.error
+            };
+            break;
+        default:
+            return {
+                hasError,
+                error
+            };
+            break;
+}
+};
+export default reducer;
